2f57293adb52fba0d28a9be2fbe5eeb58c33b312
Philipp Stucki <ps@stuckistucki.com>
2018-05-17 17:34:15 +0200

    adds canvas paint + prediction (WIP)

--- ts/src/main.ts
+++ ts/src/main.ts
@@ -1,5 +1,9 @@
 import * as tfc from '@tensorflow/tfjs-core';
-import { loadFrozenModel, NamedTensorMap } from '@tensorflow/tfjs-converter';
+import {
+    loadFrozenModel,
+    NamedTensorMap,
+    FrozenModel
+} from '@tensorflow/tfjs-converter';
 
 const MODEL_URL = '/saved_web/tensorflowjs_model.pb';
 const WEIGHTS_URL = '/saved_web/weights_manifest.json';
@@ -9,37 +13,91 @@ const IMG_SIZE_FLAT = IMG_SIZE * IMG_SIZE;
 
 const get2DContext = (el: HTMLCanvasElement) => el.getContext('2d');
 const getImageData = (ctx: CanvasRenderingContext2D) =>
-    ctx.getImageData(0, 0, 28, 28);
+    ctx.getImageData(0, 0, 400, 400);
+const getCanvasElement = () =>
+    <HTMLCanvasElement>document.getElementById('main');
+const getClearElement = () => document.getElementById('clear');
+
+const runPaint = (
+    canvasElement: HTMLCanvasElement | null,
+    clearElement: HTMLElement | null
+) => {
+    if (!canvasElement || !clearElement) {
+        throw new Error('Some elements are not defined.');
+    }
+
+    const ctx = get2DContext(canvasElement);
+    let paintStarted = false;
+
+    if (ctx) {
+        ctx.lineWidth = 40;
+        ctx.strokeStyle = '#000';
+        ctx.fillStyle = '#fff';
+
+        ctx.fillRect(0, 0, canvasElement.width, canvasElement.height);
+
+        const mouseDownHandler = (e: MouseEvent) => (paintStarted = true);
+        const mouseUpHandler = (e: MouseEvent) => (paintStarted = false);
+        const mouseMoveHandler = (e: MouseEvent) => {
+            const x = e.pageX - canvasElement.offsetLeft;
+            const y = e.pageY - canvasElement.offsetTop;
+
+            if (!paintStarted) {
+                ctx.moveTo(x, y);
+                ctx.beginPath();
+            } else {
+                ctx.lineTo(x, y);
+                ctx.stroke();
+            }
+        };
+
+        canvasElement.addEventListener('mousedown', mouseDownHandler, false);
+        canvasElement.addEventListener('mouseup', mouseUpHandler, false);
+        canvasElement.addEventListener('mouseleave', mouseUpHandler, false);
+        canvasElement.addEventListener('mousemove', mouseMoveHandler, false);
+
+        clearElement.addEventListener('click', () =>
+            ctx.fillRect(0, 0, canvasElement.width, canvasElement.height)
+        );
+    }
+};
+
+const loadModel = async () => {
+    return await loadFrozenModel(MODEL_URL, WEIGHTS_URL);
+};
+
+const predict = (model: FrozenModel, imageData: ImageData) => {
+    const resizedImage = tfc.image.resizeBilinear(tfc.fromPixels(imageData), [
+        28,
+        28
+    ]);
+
+    const x = resizedImage
+        .slice(0, [28, 28, 1])
+        .reshape([1, IMG_SIZE_FLAT])
+        .cast('float32')
+        .div(tfc.fill([1, IMG_SIZE_FLAT], 255).cast('float32'));
+
+    x.print();
+
+    const input = {
+        x: x
+    } as NamedTensorMap;
+    return (model.execute(input) as tfc.Tensor).flatten();
+};
 
 const main = async () => {
-    // tfc.setBackend('cpu');
-    console.log(tfc.getBackend());
-    const canvas = <HTMLCanvasElement>document.getElementById('main');
+    const canvas = getCanvasElement();
     const ctx = get2DContext(canvas);
-
     if (canvas && ctx) {
-        const model = await loadFrozenModel(MODEL_URL, WEIGHTS_URL);
-
-        // const xx = tfc.rand(
-        //     [1, IMG_SIZE_FLAT],
-        //     () => Math.floor(Math.random() * 256) - 1,
-        //     'float32'
-        // );
-        // xx.print();
-        // console.log(xx.shape);
-
-        const r = tfc.fromPixels(getImageData(ctx));
-
-        const input = {
-            x: r
-                .slice(0, [28, 28, 1])
-                .reshape([1, IMG_SIZE_FLAT])
-                .cast('float32')
-        } as NamedTensorMap;
-        const y = model.execute(input) as tfc.Tensor;
-        console.log(y);
-        y.print();
+        const model = await loadModel();
+        window.setInterval(() => {
+            const y = predict(model, getImageData(ctx));
+            y.argMax().print();
+            console.log(`prediction: ${y.argMax()}`);
+        }, 2000);
     }
 };
 
 main();
+runPaint(getCanvasElement(), getClearElement());
